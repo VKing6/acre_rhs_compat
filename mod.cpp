0e28095b0fd6ba879aabfb1ee9a5d07efee7a21f
Kristian Bjørge Auestad <kauestad@gmail.com>
2020-07-10 14:32:47 +0200

    Add mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,15 @@
+/// Name of your mod
+name = "ACRE2 RHS Compatibility";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+//picture = ".paa";
+/// Display next to the item added by the mod
+//logoSmall = ".paa";
+/// Logo displayed in the main menu
+//logo = ".paa";
+/// When the mouse is over, in the main menu
+//logoOver = ".paa";
+/// Website URL, that can accessed from the expansions menu
+//action = "https://github.com/VKing6/";
+/// Overview text, displayed from the extension menu
+overview = "ACRE2 compatibility mod for RHS vehicles";
+author = "VKing";
